b81a096650973b3d4b4df2ee4c906cfa082601fe
Matúš Pikuliak <matus.pikuliak@gmail.com>
2024-11-16 15:53:36 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 
 def read_requirements(filename):
     with open(filename) as f:
         return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
-
 setup(
     name="genderak",
     version="0.1",
     install_requires=read_requirements("requirements.txt"),
-    packages=find_packages(
+    packages=find_namespace_packages(
         exclude=["runs"],
     ),
 )
